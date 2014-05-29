ccaeb85fe036c693ece18d4aab96ffc56c9a8074
Andrew Marks <amracks@gmail.com>
2014-05-29 16:13:34 -0700

    vimrc and a couple syntax plugins

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,40 @@
 class vim {
 
-    package{ 'vim':
+    $plugroot = "/usr/share/vim/vimfiles/"
+
+    #Puppet path to this module
+    $modpath   = "puppet:///modules/vim"
+
+    $vimrc     = "/etc/vimrc"
+
+    package { 'vim-enhanced':
         ensure => latest
+    }
+
+    file { 'vimrc':
+        name    => $vimrc,
+        mode    => 0644,
+        owner   => 'root',
+        group   => 'root',
+        ensure  => file,
+        source  => "$modpath/vimrc"
+    }
+
+    file { 'syn-info':
+        name    => "$plugroot/syntax/info.vim",
+        mode    => 0644,
+        owner   => 'root',
+        group   => 'root',
+        ensure  => file,
+        source  => "$modpath/syntax/info.vim"
+    }
+
+    file { 'syn-man':
+        name    => "$plugroot/syntax/man.vim",
+        mode    => 0644,
+        owner   => 'root',
+        group   => 'root',
+        ensure  => file,
+        source  => "$modpath/syntax/man.vim"
     }
 }
